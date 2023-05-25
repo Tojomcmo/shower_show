cfbc84a6f5c2cf91b83075e6e8a71cbae042124a
Thomas Moriarty <tojomcmo@gmail.com>
2023-05-24 23:23:21 -0400

    added comments, added hard kill button, small refactoring

--- shower_sound_funcs.py
+++ shower_sound_funcs.py
@@ -33,12 +33,15 @@ def start_song():
     return
 
 
-#define GPIOs
-on_off_button      = gpio.Button("GPIO17")
+#define GPIO inputs
+power_button      = gpio.Button("GPIO17")
 song_change_button = gpio.Button("GPIO18")
+hard_kill          = gpio.Button("GPIO24")
+# define GPIO outputs
 shower_relay       = gpio.DigitalOutputDevice("GPIO27")
 amp_relay          = gpio.DigitalOutputDevice("GPIO22")
 disco_relay        = gpio.DigitalOutputDevice("GPIO23")
+
 
 
 # open directory of songs, pick initial song
@@ -55,64 +58,79 @@ mixer.music.set_volume(0.5)
 
 # create button filters
 song_button_filter = [0] * 30
-on_off_filter      = [0] * 30
+power_filter      = [0] * 30
 song_button_state  = False
-on_off_state       = False
+power_state       = False
 button_deadzone    = 0.9
 
 # define logic states
 prev_pressed_song_change = False
-prev_pressed_on_off      = False
+prev_pressed_power      = False
 change_song_flag         = False
 
 # set print scheduele
 print_time = 0.1
 t1 = time.time()
-while(True):
-    song_button_state,song_button_filter = button_press_filter(song_change_button,
-                                                               song_button_filter,
-                                                               button_deadzone,
-                                                               song_button_state)
-    if song_button_state:
-        if not prev_pressed_song_change: 
-            change_song_flag = True
-        prev_pressed_song_change = True
-    else:
-        prev_pressed_song_change = False
 
-    on_off_state,on_off_filter = button_press_filter(on_off_button,
-                                                     on_off_filter,
-                                                     button_deadzone,
-                                                     on_off_state)
-    if on_off_state:
-        if not prev_pressed_on_off:
-            shower_relay.on()
-            amp_relay.on()
-            disco_relay.on()
-            song_enum = pick_rand_new_song(song_enum_list, song_enum)
-            start_song()
-            change_song_flag = False
+# main loop
+while(True):
+    #check whether the kill-switch is pressed
+    if hard_kill.is_pressed:
+        exit()
+    else:    
+        # check button states with filter for noisy state change
+        song_button_state,song_button_filter = button_press_filter(song_change_button,
+                                                                song_button_filter,
+                                                                button_deadzone,
+                                                                song_button_state)
+        power_state,power_filter = button_press_filter(power_button,
+                                                        power_filter,
+                                                        button_deadzone,
+                                                        power_state)
+        
+        # music change logic
+        # if first state change and not continued press, set song change flag to true, else do nothing
+        if song_button_state:
+            if not prev_pressed_song_change: 
+                change_song_flag = True
+            prev_pressed_song_change = True
         else:
-            if change_song_flag:
+            prev_pressed_song_change = False
+
+        # power state logic
+        if power_state:
+            # if this is the initial state change to on, turn on outputs and begin song
+            if not prev_pressed_power:
+                shower_relay.on()
+                amp_relay.on()
+                disco_relay.on()
                 song_enum = pick_rand_new_song(song_enum_list, song_enum)
                 start_song()
                 change_song_flag = False
-        prev_pressed_on_off = True
-    else:
-        shower_relay.off()
-        amp_relay.off()
-        disco_relay.off()
-        mixer.music.stop()
-        prev_pressed_on_off = False    
+                prev_pressed_power = True
+            # if the state has been on, check song state    
+            else:
+                # if new song flag is true or if song has ended, pick new song and play
+                if change_song_flag or not mixer.music.get_busy():
+                    song_enum = pick_rand_new_song(song_enum_list, song_enum)
+                    start_song()
+                    change_song_flag = False
+        # if power state is off, power off outputs and stop music
+        else:
+            shower_relay.off()
+            amp_relay.off()
+            disco_relay.off()
+            mixer.music.stop()
+            prev_pressed_power = False    
 
-    time_elapsed = time.time() - t1 
-    if time_elapsed > print_time:
-        print("chg_song_flag  ,  song_num  ,  song_btn  , pwr_btn  , music_state:  ", 
-              change_song_flag, " , ", 
-              song_enum, " , ", 
-              prev_pressed_song_change, "  ,  ",
-              prev_pressed_on_off, "  ,  ",
-              mixer.music.get_busy())
-        # print("current song: ", song_enum)
-        # print("is on: ", prev_pressed_on_off)
-        t1 = time.time()
+        time_elapsed = time.time() - t1 
+        if time_elapsed > print_time:
+            print("chg_song_flag  ,  song_num  ,  song_btn  , pwr_btn  , music_state:  ", 
+                change_song_flag, " , ", 
+                song_enum, " , ", 
+                prev_pressed_song_change, "  ,  ",
+                prev_pressed_power, "  ,  ",
+                mixer.music.get_busy())
+            # print("current song: ", song_enum)
+            # print("is on: ", prev_pressed_power)
+            t1 = time.time()
